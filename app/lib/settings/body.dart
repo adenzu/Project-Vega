47c9e06438efe300bc52a76f1a8e9afb4b6bdd01
adenzu <erencakar54@gmail.com>
2021-11-09 09:42:21 +0300

    Reverted important files

--- app/lib/settings/body.dart
+++ app/lib/settings/body.dart
@@ -23,66 +23,66 @@ class _SettingsBodyState extends State<SettingsBody> {
     return ListView(
       children: [
         SwitchListTile(
-              secondary: const Icon(Icons.shield),
-              title: const Text("Yetkilendir"),
-              value: _authorized,
-              onChanged: (value) {
-                setState(() {
-                  _authorized = value;
-                });
-              },
-            ),
+          secondary: const Icon(Icons.shield),
+          title: const Text("Yetkilendir"),
+          value: _authorized,
+          onChanged: (value) {
+            setState(() {
+              _authorized = value;
+            });
+          },
+        ),
         const Divider(),
         SwitchListTile(
-              secondary: const Icon(Icons.bus_alert),
-              title: const Text("Servis yaklaşınca bildir"),
-              value: _shuttleNotify,
-              onChanged: (value) {
-                setState(() {
-                  _shuttleNotify = value;
-                });
-              },
-            ),
+          secondary: const Icon(Icons.bus_alert),
+          title: const Text("Servis yaklaşınca bildir"),
+          value: _shuttleNotify,
+          onChanged: (value) {
+            setState(() {
+              _shuttleNotify = value;
+            });
+          },
+        ),
         SwitchListTile(
-              secondary: const Icon(Icons.notifications),
-              title: const Text("Bağlı profiller güncellenince bildir"),
-              value: _childProfileNotify,
-              onChanged: (value) {
-                setState(() {
-                  _childProfileNotify = value;
-                });
-              },
-            ),
+          secondary: const Icon(Icons.notifications),
+          title: const Text("Bağlı profiller güncellenince bildir"),
+          value: _childProfileNotify,
+          onChanged: (value) {
+            setState(() {
+              _childProfileNotify = value;
+            });
+          },
+        ),
         const Divider(),
         ListTile(
-              leading: _themeIcons[_themeIndex],
-              title: const Text("Tema"),
-              trailing: Text.rich(
-                TextSpan(
-                  text: _themeNames[_themeIndex],
-                  children: const [
-                    WidgetSpan(child: Icon(Icons.arrow_drop_down)),
-                  ],
-                ),
-                textAlign: TextAlign.center,
-              ),
-              onTap: () => showTightModalBottomSheet(
-                context: context,
-                children: List.generate(
-                  _themeNames.length,
-                  (index) => ListTile(
-                    leading: _themeIcons[index],
-                    title: Text(_themeNames[index]),
-                    onTap: () {
-                      setState(() {
-                        _themeIndex = index;
-                      });
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
+          leading: _themeIcons[_themeIndex],
+          title: const Text("Tema"),
+          trailing: Text.rich(
+            TextSpan(
+              text: _themeNames[_themeIndex],
+              children: const [
+                WidgetSpan(child: Icon(Icons.arrow_drop_down)),
+              ],
+            ),
+            textAlign: TextAlign.center,
+          ),
+          onTap: () => showTightModalBottomSheet(
+            context: context,
+            children: List.generate(
+              _themeNames.length,
+              (index) => ListTile(
+                leading: _themeIcons[index],
+                title: Text(_themeNames[index]),
+                onTap: () {
+                  setState(() {
+                    _themeIndex = index;
+                  });
+                  Navigator.pop(context);
+                },
               ),
             ),
+          ),
+        ),
       ],
     );
   }
