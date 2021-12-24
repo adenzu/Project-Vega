cb177b35f3f58f598a1ced3ef96614c5089926f7
adenzu <erencakar54@gmail.com>
2021-12-24 15:42:46 +0300

    database değiştirme fonksiyonları

--- app/lib/database/functions.dart
+++ app/lib/database/functions.dart
@@ -1,4 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+
+/// verilen shuttle id'yi kullanıcının servislerine ekler
+///
+/// NOT: bu fonksiyon kullanıcıyı servise abone etmez!
+///      kullanıcıyı shuttleEmployees altına alıp shuttle'ı da
+///      servisi olarak ekler
+Future<void> addShuttle(String shuttleId) async {
+  String userId = FirebaseAuth.instance.currentUser!.uid;
+  FirebaseDatabase.instance
+      .reference()
+      .child("shuttleEmployees/$userId/shuttles/$shuttleId")
+      .set(true);
+}
+
+/// verilen shuttle id'yi kullanıcının servislerinden siler
+///
+/// NOT: bu fonksiyon kullanıcının servis aboneliğini silmez!
+///      shuttleEmployees altındaki kullanıcının servislerinden
+///      verilen id'yi siler
+Future<void> removeShuttle(String shuttleId) async {
+  String userId = FirebaseAuth.instance.currentUser!.uid;
+  FirebaseDatabase.instance
+      .reference()
+      .child("shuttleEmployees/$userId/shuttles/$shuttleId")
+      .set(null);
+}
 
 /// verilen id'li userı verilen id'li suhttle'a abone eder
 /// başarılı olursa `true` olmazsa `false` döner
@@ -22,10 +49,35 @@ Future<void> removeFromShuttle(String userId, String shuttleId) async {
       .set(null);
 }
 
+/// verilend id'yi kullanıcının bağlı (children) profillerine ekler
+Future<void> addChild(String childId) async {
+  String userId = FirebaseAuth.instance.currentUser!.uid;
+  await FirebaseDatabase.instance
+      .reference()
+      .child("users/$userId/children/$childId")
+      .set(true);
+}
+
+/// verilend id'yi kullanıcının bağlı (children) profillerinden siler
+Future<void> removeChild(String childId) async {
+  String userId = FirebaseAuth.instance.currentUser!.uid;
+  await FirebaseDatabase.instance
+      .reference()
+      .child("users/$userId/children/$childId")
+      .set(null);
+}
 
 /// hiyerarşi:
 ///```
 /// database
+///   shuttleEmployees
+///     employeeId
+///       shuttles
+///         shuttleId
+///   users
+///     userId
+///       children
+///         childId
 ///   shuttles
 ///     shuttleId
 ///       passengers
